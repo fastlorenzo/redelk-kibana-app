5acd3a84e716dff89c4a8906bbac8a31a898584b
Lorenzo Bernardi <git@bernardi.be>
2021-04-01 11:50:03 +0200

    Updated templates to latest version
    Signed-off-by: Lorenzo Bernardi <git@bernardi.be>

---
+++ public/components/healthPage.tsx
@@ -0,0 +1,65 @@
+/*
+ * Part of RedELK
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020, Lorenzo Bernardi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Authors:
+ * - Lorenzo Bernardi
+ */
+
+import React from 'react';
+import {useTopNav} from "../helpers/nav_header_helper";
+import {EmbeddedDashboard} from "./embeddedDashboard";
+import {useKibana} from '../../../../src/plugins/kibana_react/public';
+import {TopNavMenuData} from '../../../../src/plugins/navigation/public';
+import {RedelkKibanaService} from "../types";
+
+export const ImplantsPage = () => {
+
+  useTopNav(true);
+
+  const {services}: { services: RedelkKibanaService } = useKibana();
+
+  const discoverTopNav: TopNavMenuData = {
+    id: "go-to-discover",
+    label: "Open in discover app",
+    run: () => {
+      services.application?.navigateToApp('discover', {path: "#/view/68e8e870-b6aa-11e8-bc1a-cf8fa3255855"})
+    }
+  }
+
+  return (
+    <>
+      <EmbeddedDashboard dashboardId="117dbba0-c6f5-11e8-a9c6-cd307b96b1ba" extraTopNavMenu={[discoverTopNav]}/>
+    </>
+  );
+};
